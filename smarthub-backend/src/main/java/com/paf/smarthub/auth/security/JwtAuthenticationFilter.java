41fb4ac524d6a697e4de16f350f6aaedd6244c85
sachzindu <pasindugreencode@gmail.com>
2026-04-22 19:16:58 +0530

    Fixed an issue with JwtAuth

--- smarthub-backend/src/main/java/com/paf/smarthub/auth/security/JwtAuthenticationFilter.java
+++ smarthub-backend/src/main/java/com/paf/smarthub/auth/security/JwtAuthenticationFilter.java
@@ -1,3 +1,4 @@
+package com.paf.smarthub.auth.security;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -11,8 +12,8 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
-
 import java.io.IOException;
+
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
